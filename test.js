e7c48dcb64d190cf52a198f424932fc82c74d479
Lucasvegaa <lucasvegaa@gmail.com>
2022-03-31 14:20:14 -0300

    fix error

--- test.js
+++ test.js
@@ -2,17 +2,24 @@ const Contenedor = require ('./contenedor.js')
 
 const archivo = new Contenedor('productos')
 
+archivo.getAll().then(x=>console.log(x))
 archivo.save({
-    title: 'Carpetas',
-    price: 100.2,
-    thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png'
-}
-)
+    "title": "Escuadra",
+    "price": 123.45,
+    "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png",
+    "id": 1
+  })
+  archivo.save({
+    "title": "Escuadra",
+    "price": 223.45,
+    "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png",
+    "id": 1
+  })
+  archivo.save({
+    "title": "Escuadra",
+    "price": 323.45,
+    "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png",
+    "id": 1
+  })
+archivo.getById("4").then(x=>console.log("el objeto es :",x))
 
-archivo.getById('1');
-
-archivo.getAll();
-
-archivo.deleteById('1')
-
-archivo.deleteAll();
